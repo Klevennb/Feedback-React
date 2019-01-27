d1218a725a269ee9b0f15e6c356c7156d5b3cb30
Matt Kleven <klevennb115@gmail.com>
2019-01-26 20:02:17 -0600

    Added Feedback

--- src/components/FeelingFeedback/FeelingFeedback.js
+++ src/components/FeelingFeedback/FeelingFeedback.js
@@ -3,7 +3,12 @@ class FeelingFeedback extends Component {
     render() {
         return (
             <div>
-
+                <h2>How are you feeling today?</h2>
+                <br />
+                <div>
+                    <h5>Feeling?</h5>
+                    <input></input>
+                </div>
             </div>
         );
     }
